802160552793cb404c7fe4039b3669ed31d30dd2
Svante Pihl <hej@svantepihl.com>
2021-03-01 18:33:37 +0100

    Del 1 & 3 klara. Påbörjat del 2.

--- LaborationCSN/Controllers/CSNController.cs
+++ LaborationCSN/Controllers/CSNController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace LaborationCSN.Controllers
 
         public CSNController()
         {
-            string path = HostingEnvironment.MapPath("/db/");
+            var path = HostingEnvironment.MapPath("/db/");
             sqlite = new SQLiteConnection($@"DataSource={path}\csn.sqlite");
 
         }
@@ -26,7 +27,7 @@ namespace LaborationCSN.Controllers
             var adapt = new SQLiteDataAdapter(query, sqlite);
             var ds = new DataSet(root);
             adapt.Fill(ds, nodeName);
-            XElement xe = XElement.Parse(ds.GetXml());
+            var xe = XElement.Parse(ds.GetXml());
 
             sqlite.Close();
             return xe;
@@ -41,14 +42,14 @@ namespace LaborationCSN.Controllers
         // Lite överkomplicerat för just detta enkla fall men visar på idén.
         public ActionResult Test()
         {
-            string query = @"SELECT a.Arendenummer, s.Beskrivning, SUM(((Sluttid-starttid +1) * b.Belopp)) as Summa
+            var query = @"SELECT a.Arendenummer, s.Beskrivning, SUM(((Sluttid-starttid +1) * b.Belopp)) as Summa
                             FROM Arende a, Belopp b, BeviljadTid bt, BeviljadTid_Belopp btb, Stodform s, Beloppstyp blt
                             WHERE a.Arendenummer = bt.Arendenummer AND s.Stodformskod = a.Stodformskod
                             AND btb.BeloppID = b.BeloppID AND btb.BeviljadTidID = bt.BeviljadTidID AND b.Beloppstypkod = blt.Beloppstypkod AND b.BeloppID LIKE '%2009'
 							Group by a.Arendenummer
 							Order by a.Arendenummer ASC";
-            XElement test = SQLResult(query, "BeviljadeTider2009", "BeviljadTid");
-            XElement summa = new XElement("Total",
+            var test = SQLResult(query, "BeviljadeTider2009", "BeviljadTid");
+            var summa = new XElement("Total",
                 (from b in test.Descendants("Summa")
                  select (int)b).Sum());
             test.Add(summa);
@@ -73,51 +74,52 @@ namespace LaborationCSN.Controllers
 
         public ActionResult Uppgift1()
         {
-            string query =
-                @"SELECT Arende.Arendenummer, Stodform.Beskrivning, Utbetalning.UtbetDatum, Utbetalning.UtbetStatus, sum(Belopp.Belopp*(UtbetaldTid.Sluttid - UtbetaldTid.StartTid+1)) as Belopp
-                            FROM Utbetalning
-                            LEFT JOIN Utbetalningsplan on Utbetalningsplan.UtbetPlanID = Utbetalning.UtbetPlanID
-                            LEFT JOIN Arende on Arende.Arendenummer = Utbetalningsplan.Arendenummer
-                            LEFT JOIN UtbetaldTid on UtbetaldTid.UtbetID = Utbetalning.UtbetID
-                            LEFT JOIN UtbetaldTid_Belopp on UtbetaldTid_Belopp.UtbetaldTidID = UtbetaldTid.UtbetTidID
-                            LEFT JOIN Belopp on Belopp.BeloppID = UtbetaldTid_Belopp.BeloppID
-                            LEFT JOIN Stodform on Stodform.Stodformskod = Arende.Stodformskod
-                            GROUP BY Arende.Arendenummer,Utbetalning.UtbetDatum
-                            ORDER BY Arende.Arendenummer";
+            var query =
+                @"SELECT UtbetDatum as Datum, Beloppstyp.Beskrivning, sum(Belopp.Belopp*(UtbetaldTid.Sluttid - UtbetaldTid.StartTid+1)) as Belopp
+	                FROM Utbetalning
+	                LEFT JOIN Utbetalningsplan on Utbetalningsplan.UtbetPlanID = Utbetalning.UtbetPlanID
+	                LEFT JOIN Arende on Arende.Arendenummer = Utbetalningsplan.Arendenummer
+	                LEFT JOIN UtbetaldTid on UtbetaldTid.UtbetID = Utbetalning.UtbetID
+	                LEFT JOIN UtbetaldTid_Belopp on UtbetaldTid_Belopp.UtbetaldTidID = UtbetaldTid.UtbetTidID
+	                LEFT JOIN Belopp on Belopp.BeloppID = UtbetaldTid_Belopp.BeloppID
+	                LEFT JOIN Stodform on Stodform.Stodformskod = Arende.Stodformskod
+	                LEFT JOIN Beloppstyp on Beloppstyp.Beloppstypkod = Belopp.Beloppstypkod
+	                GROUP BY Utbetalning.UtbetDatum,Belopp.Beloppstypkod
+	                ORDER BY Utbetalning.UtbetDatum";
             
-            XElement raw = SQLResult(query, "AllArenden", "Utbetalning");
+            var raw = SQLResult(query, "AllArenden", "Utbetalning");
 
             var groups = raw.Descendants("Utbetalning")
                 .GroupBy(x => (string)x.Element("Arendenummer"))
                 .ToList();
 
-            XElement result = new XElement("UtbetalningarPerArende");
+            var result = new XElement("UtbetalningarPerArende");
             
             foreach (var group in groups)
             {
-                XElement arendeXElement = new XElement("Arende", new XAttribute("Arendenummer", group.Key));
+                var arendeXElement = new XElement("Arende", new XAttribute("Arendenummer", group.Key));
                 group.ToList().ForEach(x=>arendeXElement.Add(x));
-                XElement totalSumma = new XElement("TotalSumma",
+                var totalSumma = new XElement("TotalSumma",
                     arendeXElement.Descendants("Belopp")
                         .Select(x => (int) x)
                         .Sum());
                 arendeXElement.Add(totalSumma);
                 
-                XElement UtbetaldSumma = new XElement("UtbetaldSumma",
+                var UtbetaldSumma = new XElement("UtbetaldSumma",
                     arendeXElement.Elements()
                         .Where(el => el.Element("UtbetStatus")?.Value == "Utbetald")
                         .Select(el => (int) el.Element("Belopp"))
                         .Sum());
                 arendeXElement.Add(UtbetaldSumma);
                 
-                XElement KvarvarandeSumma = new XElement("KvarvarandeSumma",
+                var KvarvarandeSumma = new XElement("KvarvarandeSumma",
                     arendeXElement.Elements()
                         .Where(el => el.Element("UtbetStatus")?.Value == "Planerad")
                         .Select(el => (int) el.Element("Belopp"))
                         .Sum());
                 arendeXElement.Add(KvarvarandeSumma);
 
-                XElement Beskrivning = new XElement("Beskrivning", arendeXElement.Descendants("Beskrivning").First().Value);
+                var Beskrivning = new XElement("Beskrivning", arendeXElement.Descendants("Beskrivning").First().Value);
                 arendeXElement.Add(Beskrivning);
                 
                 result.Add(arendeXElement);
@@ -134,6 +136,40 @@ namespace LaborationCSN.Controllers
 
         public ActionResult Uppgift2()
         {
+         
+            var query =
+                @"SELECT Utbetalning.UtbetDatum, Beloppstyp.Beskrivning, Belopp.Belopp*(UtbetaldTid.Sluttid - UtbetaldTid.StartTid+1) as Belopp
+                    FROM Utbetalning
+                    LEFT JOIN Utbetalningsplan on Utbetalningsplan.UtbetPlanID = Utbetalning.UtbetPlanID
+                    LEFT JOIN UtbetaldTid on UtbetaldTid.UtbetID = Utbetalning.UtbetID
+                    LEFT JOIN UtbetaldTid_Belopp on UtbetaldTid_Belopp.UtbetaldTidID = UtbetaldTid.UtbetTidID
+                    LEFT JOIN Belopp on Belopp.BeloppID = UtbetaldTid_Belopp.BeloppID
+                    LEFT JOIN Beloppstyp on Beloppstyp.Beloppstypkod = Belopp.Beloppstypkod
+                    ORDER BY Utbetalning.UtbetDatum
+                    ";
+            
+            var raw = SQLResult(query, "AllaDatum", "Betalning");
+            
+            var groups = raw.Descendants("Betalning")
+                .GroupBy(x => (string)x.Element("UtbetDatum"))
+                .ToList();
+
+            var result = new XElement("UtbetalningarPerDatum");
+
+            foreach (var group in groups)
+            {
+                var datumElement = new XElement("UtbetalningsDag", new XAttribute("Datum", group.Key));
+                group.ToList().ForEach(x=>datumElement.Add(x));
+                
+                var totalSumma = new XElement("TotalSumma",
+                    datumElement.Descendants("Belopp")
+                        .Select(x => (int) x)
+                        .Sum());
+                datumElement.Add(totalSumma);
+                result.Add(datumElement);
+            }
+
+            Console.WriteLine(result.ToString());
             return View();
         }
 
@@ -142,7 +178,18 @@ namespace LaborationCSN.Controllers
 
         public ActionResult Uppgift3()
         {
-            return View();
+            var query =
+                @"SELECT BeviljadTid.Starttid as Startdatum, BeviljadTid.Sluttid as Slutdatum, Stodform.Beskrivning as Typ, sum(Belopp.Belopp*(BeviljadTid.Sluttid - BeviljadTid.StartTid+1)) as Belopp
+                        FROM BeviljadTid
+                        LEFT JOIN BeviljadTid_Belopp on BeviljadTid_Belopp.BeviljadTidID = BeviljadTid.BeviljadTidID
+                        LEFT JOIN Arende on Arende.Arendenummer = BeviljadTid.Arendenummer
+                        LEFT JOIN Stodform on Stodform.Stodformskod = Arende.Stodformskod
+                        LEFT JOIN Belopp on Belopp.BeloppID = BeviljadTid_Belopp.BeloppID
+                        GROUP BY Arende.Arendenummer, BeviljadTid.Starttid";
+            
+            var result = SQLResult(query, " BeviljadeTider", "TidsPeriod");
+            
+            return View(result);
         }
     }
 }
